1c956161c811c393a56807d892fa6e2b2296a48b
alexlofton <147463152+alexlofton@users.noreply.github.com>
2023-11-27 17:41:42 -0700

    added localstorage and updated js

--- assets/script.js
+++ assets/script.js
@@ -5,42 +5,95 @@ var APIKey = "271380b0bfa8a33cb1d80c12040623cc";
 var searchInput = document.querySelector('#search-city');
 var searchBtn = document.querySelector('#search-button');
 var searchForm = document.querySelector('#user-form');
-var currentCity = document.querySelector('.current-city-header')
+var currentCity = document.querySelector('.current-city-header');
 var currentWeatherContent = document.querySelector('#current-weather-container');
-var forecastContent = document.querySelector('#forecast');
+var forecastContent = document.querySelector('#forecast-container');
+var searchHistory = document.querySelector('#history');
 
 function printResults(currentWeather, fiveDayForecasts) {
-    var testArray = ["dog", "cat", "bird"]
-    console.log(testArray)
-    console.log(currentWeather, fiveDayForecasts)
+    var date = new Date();
+
+    var options = { 
+        year: 'numeric', 
+        month: '2-digit', 
+        day: '2-digit' 
+    };
+
+    var formattedCurrentDate = date.toLocaleDateString('en-US', options);
+
     //current weather
     if (currentWeather.main) {
-        currentWeatherContent.innerHTML += '<p><strong>Temp:</strong> ' + currentWeather.main.temp + '</p>'
-        currentWeatherContent.innerHTML += '<p><strong>Feels Like:</strong> ' + currentWeather.main.feels_like + '</p>'
-        currentWeatherContent.innerHTML += '<p><strong>Humidity:</strong> ' + currentWeather.main.humidity + '</p>'
+        currentCity.textContent = currentWeather.name;
+
+        var currentDateEl = document.createElement('h2');
+        currentDateEl.textContent = formattedCurrentDate;
+        currentCity.append(currentDateEl);
+
+        var currentForecastIcon = document.createElement('img');
+        currentForecastIcon.setAttribute("src", `http://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`);
+        currentCity.append(currentForecastIcon);
+
+        currentWeatherContent.innerHTML += '<p><strong>Temp:</strong> ' + Math.round(currentWeather.main.temp) + 'F' + '</p>'
+
+        currentWeatherContent.innerHTML += '<p><strong>Feels Like:</strong> ' + Math.round(currentWeather.main.feels_like) + 'F' + '</p>'
+
+        currentWeatherContent.innerHTML += '<p><strong>Humidity:</strong> ' + currentWeather.main.humidity + '%' + '</p>'
     }
+
     //five day forecast
-    // console.log('hittin')
-    // return fiveDayForecasts.map(function(forecast) {
-    //     console.log(forecast)
-        // return forecast.main.temp
-    // })
+    return fiveDayForecasts.forEach(function(forecast) {
+        var date = new Date(forecast.dt_txt);
+
+        var formattedDate = date.toLocaleDateString('en-US', options);
+
+        var resultCard = document.createElement('div');
+        resultCard.classList.add('test-outer-div', 'border', 'border-primary')
+
+        var resultBody = document.createElement('div');
+        resultBody.classList.add('test-inner-div')
+        resultCard.append(resultBody);
+
+        var dateEl = document.createElement('h3');
+        dateEl.classList.add('h3-date')
+        dateEl.textContent = formattedDate;
+        resultBody.append(dateEl);
+
+        var forecastIcon = document.createElement('img');
+        forecastIcon.setAttribute("src", `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`);
+        resultBody.append(forecastIcon);
+
+        var maxTemp = document.createElement('h6');
+        maxTemp.innerHTML = '<strong>High: </strong>' + Math.round(forecast.main.temp_max) + 'F' + '<br/>';
+        resultBody.append(maxTemp);
+
+        var minTemp = document.createElement('h6');
+        minTemp.innerHTML = '<strong>Low: </strong>' + Math.round(forecast.main.temp_min) + 'F' + '<br/>';
+        resultBody.append(minTemp);
+
+        var humidity = document.createElement('h6');
+        humidity.innerHTML = '<strong>Humidity: </strong>' + Math.round(forecast.main.humidity) + '%' + '<br/>';
+        resultBody.append(humidity);
+
+        return forecastContent.append(resultCard);
+    })
 }
 
-function searchApi(city, APIKey) {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+async function searchApi(city, APIKey) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
     var longitude = "";
     var latitude = "";
+    
+    // console.log(APIKey)
+    await fetch(queryURL)
+    .then(function (response) {
+        console.log(response)
+        if (!response.ok) {
+            throw response.json();
+        }
+        return response.json();
+    })
 
-    fetch(queryURL)
-        .then(function (response) {
-            if (!response.ok) {
-                throw response.json();
-            }
-            return response.json();
-        })
         .then(function (locRes) {
-            currentCity.textContent = locRes.name
             if (!locRes) {
                 console.log('No results found');
                 currentWeatherContent.innerHTML = '<h3>No results found</h3>';
@@ -50,14 +103,16 @@ function searchApi(city, APIKey) {
                 latitude = locRes.coord.lat.toString();
                 searchForecastAPI(longitude, latitude, locRes);
             }
+            return searchForecastAPI
         })
-    }
-    
-    function searchForecastAPI(longitude, latitude, currentWeatherResp) {
-        var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
-        var fiveDayForecasts = [];
-    
-    fetch(forecastQueryURL)
+}
+
+async function searchForecastAPI(longitude, latitude, currentWeatherResp) {
+    var forecastQueryURL = "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
+    var fiveDayForecasts = [];
+
+    // console.log(APIKey)
+    await fetch(forecastQueryURL)
         .then(function (response) {
             if (!response.ok) {
                 throw response.json();
@@ -65,31 +120,66 @@ function searchApi(city, APIKey) {
             return response.json();
         })
         .then(function (locRes) {
-            return locRes.list.filter(function(item) {
+            locRes.list.filter(function(item) {
                 var formattedTime = item.dt_txt.slice(11)
 
                 if (formattedTime === '12:00:00') {
                     fiveDayForecasts.push(item)
                 }
-                return fiveDayForecasts
             })
+            return fiveDayForecasts
         })
-        console.log(fiveDayForecasts)
+
         printResults(currentWeatherResp, fiveDayForecasts)
 }
 
+//setting search history to localStorage
+function setLocalStorage(city) {
+    localStorage.setItem(city, city);
+
+    var searchHistoryItem = document.createElement('button')
+    searchHistoryItem.textContent = city;
+    searchHistory.append(searchHistoryItem);
+}
+
+function getUserInput() {
+    var storageKeys = Object.keys(localStorage);
+
+    for (var i = 0; i < storageKeys.length; i++) {
+
+    var key = storageKeys[i];
+    var retrievedUserInput = localStorage.getItem(key);
+    
+    var searchHistoryItem = document.createElement('button');
+    searchHistoryItem.textContent = retrievedUserInput;
+    searchHistory.append(searchHistoryItem);
+
+      // add a click event listener to each button
+    searchHistoryItem.addEventListener('click', searchApi(retrievedUserInput))
+    // console.log(APIKey)
+
+        // Define the action to perform when the button is clicked
+        console.log('Button clicked: ' + retrievedUserInput);
+    };
+    }
+
+
 function handleSearchFormSubmit(event) {
     event.preventDefault();
-
+    
     var city = searchInput.value;
-    console.log("testing")
-
+    
     if (!city) {
         console.error('Please enter valid city');
         return;
     }
-
+    
+    setLocalStorage(city);
     searchApi(city, APIKey);
 }
+
+document.querySelector('#body').onload = function(){
+    getUserInput();
+};
 
 searchForm.addEventListener('submit', handleSearchFormSubmit);
